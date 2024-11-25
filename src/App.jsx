b2ac2497d5589cebe8c115ceb08603cafebe5c51
akivaab <amabramo18@gmail.com>
2024-11-25 14:33:07 +0200

    added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import Header from "./Header"
 import PostList from "./PostList"
 import CreatePost from "./CreatePost"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
   const [posts, setPosts] = useState(JSON.parse(localStorage.getItem('posts')) || [])
@@ -14,12 +15,15 @@ function App() {
   const deletePost = deletedPost => setPosts(prevPosts => prevPosts.filter(post => post.date !== deletedPost.date))
 
   return (
-    <div>
-      <Header />
-      <PostList posts={posts} onDelete={deletePost}/>
-      <hr />
-      <CreatePost onCreate={createPost}/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<PostList posts={posts} onDelete={deletePost}/>} />
+          <Route path="/create" element={<CreatePost onCreate={createPost}/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return ( 
     <header className="bg-green-800 text-white p-5 mb-1">
       <div className="container mx-auto flex items-center justify-between">
         <h1 className="text-3xl font-bold">CrudBlog</h1>
         <nav className="flex space-x-8">
-          <a href="/" className="text-white hover:text-gray-300 transition-colors duration-200">Home</a>
-          <a href="/create" className="text-white hover:text-gray-300 transition-colors duration-200">Write Blog</a>
+          <Link to="/" className="text-white hover:text-gray-300 transition-colors duration-200">Home</Link>
+          <Link to="/create" className="text-white hover:text-gray-300 transition-colors duration-200">Write Blog</Link>
         </nav>
       </div>
     </header>
