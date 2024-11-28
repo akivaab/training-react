1ca0a6774a771973862fb77db0ba1f55c52e866e
akivaab <amabramo18@gmail.com>
2024-11-28 12:05:40 +0200

    and tailwind for thee as well

--- src/Error.jsx
+++ src/Error.jsx
@@ -2,9 +2,16 @@ import { Link } from "react-router-dom";
 
 function Error() {
   return (
-    <div>
-      <h1>Page not found</h1>
-      <Link to="/">Back to Home</Link>
+    <div className="mx-auto max-w-3xl p-4 text-center">
+      <h1 className="p-2 text-3xl text-slate-800">Page Not Found</h1>
+      <h2 className="p-2 text-xl text-slate-800">
+        Sorry, but we could not find the page you are looking for.
+      </h2>
+      <Link to="/">
+        <button className="mt-4 rounded-2xl bg-green-800 p-3 text-white shadow transition-shadow hover:shadow-lg">
+          Back to Home
+        </button>
+      </Link>
     </div>
   );
 }
--- src/PostDetails.jsx
+++ src/PostDetails.jsx
@@ -39,12 +39,12 @@ function PostDetails({ posts, onDelete, onUpdate }) {
   };
 
   return (
-    <div>
+    <div className="mx-auto max-w-3xl p-4 text-left">
       {isUpdating ? (
         <div>
           <form onSubmit={handleSubmit}>
             <input
-              className="block w-full bg-green-100"
+              className="mb-4 block w-full rounded-sm border border-slate-400 bg-green-50 p-1 text-2xl font-medium"
               type="text"
               required
               placeholder="Title"
@@ -52,7 +52,7 @@ function PostDetails({ posts, onDelete, onUpdate }) {
               onChange={(e) => setTitle(e.target.value)}
             />
             <input
-              className="block w-full bg-green-100"
+              className="mb-4 block w-full rounded-sm border border-slate-400 bg-green-50 p-1 text-lg font-normal"
               type="text"
               required
               placeholder="Author"
@@ -60,26 +60,51 @@ function PostDetails({ posts, onDelete, onUpdate }) {
               onChange={(e) => setAuthor(e.target.value)}
             />
             <textarea
-              className="block w-full bg-green-100"
+              className="mb-4 block h-80 w-full rounded-sm border border-slate-400 bg-green-50 p-1"
               required
               value={body}
               placeholder="Blog Post"
               onChange={(e) => setBody(e.target.value)}
             ></textarea>
-            <button className="bg-green-800 text-white" type="submit">
+            <button
+              className="mx-auto mt-4 block rounded-2xl bg-green-800 p-3 text-white shadow transition-shadow hover:shadow-lg"
+              type="submit"
+            >
               Save Changes
             </button>
           </form>
         </div>
       ) : (
-        <div>
-          <h2>{title}</h2>
-          <h3>Written by {author}</h3>
-          <h3>Posted {post.date}</h3>
-          <p>{body}</p>
-          <button onClick={handleDelete}>Delete</button>
-          <button onClick={() => setIsUpdating(true)}>Edit</button>
-        </div>
+        <>
+          <div className="mx-2 flex items-start justify-between">
+            <div className="mb-3 p-1">
+              <h2 className="pb-1 text-2xl font-medium text-green-700">
+                {title}
+              </h2>
+              <h3 className="text-lg font-normal text-slate-700">
+                Written by {author}
+              </h3>
+              <h3 className="text-lg font-normal text-slate-700">
+                Posted {post.date}
+              </h3>
+            </div>
+            <div className="my-auto space-x-3">
+              <button
+                className="w-24 rounded-2xl bg-green-800 p-3 text-white shadow transition-shadow hover:shadow-lg"
+                onClick={() => setIsUpdating(true)}
+              >
+                Edit
+              </button>
+              <button
+                className="w-24 rounded-2xl bg-red-800 p-3 text-white shadow transition-shadow hover:shadow-lg"
+                onClick={handleDelete}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+          <p className="text-slate-950">{body}</p>
+        </>
       )}
     </div>
   );
